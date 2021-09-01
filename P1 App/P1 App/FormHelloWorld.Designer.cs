a7ef31fb53bc3870ac2bf0f72d5e63af02217cdd
shiannbaker <shiann.baker@trojans.dsu.edu>
2021-08-31 21:13:34 -0500

    update exit button

--- P1 App/P1 App/FormHelloWorld.Designer.cs
+++ P1 App/P1 App/FormHelloWorld.Designer.cs
@@ -30,23 +30,40 @@ namespace P1_App
         private void InitializeComponent()
         {
             this.LabelHelloWorld = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // LabelHelloWorld
             // 
             this.LabelHelloWorld.AutoSize = true;
-            this.LabelHelloWorld.Location = new System.Drawing.Point(12, 9);
+            this.LabelHelloWorld.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelHelloWorld.Location = new System.Drawing.Point(72, 30);
+            this.LabelHelloWorld.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.LabelHelloWorld.Name = "LabelHelloWorld";
-            this.LabelHelloWorld.Size = new System.Drawing.Size(88, 17);
+            this.LabelHelloWorld.Size = new System.Drawing.Size(212, 39);
             this.LabelHelloWorld.TabIndex = 0;
             this.LabelHelloWorld.Text = "Hello, World!";
+            this.LabelHelloWorld.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(268, 83);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 39);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Exit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // FormHelloWorld
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(415, 450);
+            this.ClientSize = new System.Drawing.Size(369, 134);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.LabelHelloWorld);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FormHelloWorld";
             this.Text = "CSC 470 - P1";
             this.Load += new System.EventHandler(this.FormHelloWorld_Load);
@@ -58,6 +75,7 @@ namespace P1_App
         #endregion
 
         private System.Windows.Forms.Label LabelHelloWorld;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- P1 App/P1 App/FormHelloWorld.cs
+++ P1 App/P1 App/FormHelloWorld.cs
@@ -27,7 +27,7 @@ namespace P1_App
                                       (Screen.PrimaryScreen.WorkingArea.Height - this.Height) / 2);
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void button1_Click_1(object sender, EventArgs e)
         {
             this.Close();
         }
